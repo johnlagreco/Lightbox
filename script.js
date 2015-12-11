dc759313f861f3883ed8a14e6562d5b43cc294fe
John LaGreco <johnlagreco@gmail.com>
2015-12-11 15:29:52 -0500

    added images and gave attributes

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 $(document).ready(
 	function(){
 
-	$(".lightbox-link").on("click",function(){
-		console.log("you have clicked");
+	$(".lightbox-link").on("click",function(e){
+		e.preventDefault();
+		var $image = $("#this-image");
+		$image.attr("src", $(this).attr("src"));
 		$(".outer-lightbox").show();
 		$(".outer-lightbox").on("click",function(){
 			$(".outer-lightbox").hide();
-		})
+		});
 
-	})	
+	});	
 });
