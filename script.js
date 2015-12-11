15702536df730e8c5cffafc4a2cfbf513cc4780f
John LaGreco <johnlagreco@gmail.com>
2015-12-11 12:29:16 -0500

    created toggle

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
-$(document).ready(function)(){
-	//code goes here
+$(document).ready(
+	function(){
 
-		
+	$(".lightbox-link").on("click",function(){
+		console.log("you have clicked");
+		$(".outer-lightbox").show();
+		$(".outer-lightbox").on("click",function(){
+			$(".outer-lightbox").hide();
+		})
+
+	})	
 });
